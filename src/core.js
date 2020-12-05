ab701e801a6d238b01e9139bfa52b649c8a8fd32
Phil Pluckthun <phil@kitten.sh>
2020-12-05 06:28:28 +0000

    Update export and README

--- src/core.js
+++ src/core.js
@@ -46,7 +46,7 @@ export const parse = (pattern) => (input) => {
   return pattern(state);
 };
 
-const match = (name, transform) => (quasis, ...expressions) => {
+export const match = (name, transform) => (quasis, ...expressions) => {
   const ast = parseDSL(
     quasis,
     expressions.map((expression, i) => `_exec(state, _e${i})`)
@@ -60,5 +60,3 @@ const match = (name, transform) => (quasis, ...expressions) => {
   );
   return makeMatcher(_exec, name, transform, ...expressions.map(_pattern));
 };
-
-export default match;
